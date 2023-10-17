42ae7a746308ae957a2cf081caad3b4e57695e3b
Jjomyi <rkdxk1648@naver.com>
2023-10-17 12:15:02 +0900

    백엔드,프론트 전체적 수정
    백엔드 -> sql 구문 수정
    프론트 -> async await를 이용하여 코드를 수정

--- client/src/App.js
+++ client/src/App.js
@@ -5,89 +5,48 @@ import './App.css';
 function App() {
   const [name,setName] = useState('')
   const [result,setResult] = useState([])
-  const [newdata,setNewdata] = useState('')
 
   const onChange = (e) => {
-    // console.log(e.target.value)
     setName(e.target.value)
   }
-  const onClick = (e) => {
-      const URL = 'http://localhost:8080/userinfo'
-      axios.get(URL)
-        .then(res => {
-          let found = false
-          const newResult = []
-          // console.log(res.data[0].createdDate)
-          // console.log(res.data[0].playid)
-          for(let a = 0; a<res.data.length; a++) {
-            if(name === res.data[a].playid) {
-              console.log(res.data[a].name)
-              console.log(res.data[a].name2)
-              newResult.push(res.data[a].name)
-              newResult.push(res.data[a].name2)
-              // setResult(res.data[a].name)
-              // setResult(res.data[a].name2)
-              // const mysqlDate = res.data[a].createdDate
-              // const dataobj = new Date(mysqlDate)
-              // const year = dataobj.getFullYear()
-              // const month = String(dataobj.getMonth() + 1).padStart(2,'0')
-              // const day = String(dataobj.getDate()).padStart(2,'0')
-              // const formattedDate = `${year}-${month}-${day}`
-              // console.log(formattedDate)
-              // setNewdata(formattedDate)
 
-              found = true
-              break;
-            }
-          }
-          if(!found) {
-            alert(`${name}은 없는 닉네임이야`)
-          }
-          console.log(newResult)
-          // setResult(newResult)
-        }
-        )
+  // 1. 개선점 => name을 api에 넘겨줘야함 (query params를 써서)
+  const fetchData = async () => {
+    try {
+      const response = await axios.get(`http://localhost:8080/api/username?name=${name}`)
+      setResult(response.data)
+    }
+    catch(error) {
+      console.log('데이터를 가져오는 중 에러 발생 : ',error)
+    }
   }
-  const resetonClick = () => {
-    console.log('초기화')
-    setName('')
-    setResult([])
-    setNewdata('')
+  const onClick = () => {
+    fetchData()
   }
-  // 클릭을 했을때 
-  // userinfo 데이터베이스 name에서
-  // name === api 닉네임이 같을때
-  // 출력해준다
-  //수집일 출력하기 9월4일 아직 하는중
-
-  return (
+  return(
     <>
-    <div>별놈돋보기</div>
-    
     <div>닉변이력 검색기</div>
-      <div>
-        <div>조회결과</div>
-        <input 
-        type = "text"
+    <div>
+      <div>조회결과</div>
+      <input
+        type="text"
         placeholder='예전 닉 검색 가능'
-        value = {name}
+        value={name}
         onChange={onChange}
-        />
-        <button onClick={onClick}> 검색 </button>
-        <button onClick={resetonClick}>초기화</button>
-        <div>닉네임</div>
-        {/* <div>{result.map((result) => (
-          <li key = {result.playid}>{result.name}</li>
-        ))}</div> */}
-        {/* <div>{result}</div> */}
-        {/* {result.map((name,index) => (
-          <div key = {index}>{name}</div>
-        ))} */}
-        <div>수집일</div>
-        <div>{newdata}</div>
+      />
+      <button onClick={onClick}>검색</button>
+      <div>닉네임</div>
+      <div>{result.map((item => 
+      <div key={item.playid}>
+        <div>{item.name}</div>
+        <div>{item.createdDate}</div>
       </div>
+      ))}
+      </div>
+    </div>
     </>
-  );
+  )
 }
 
-export default App;
+export default App
+
--- client/src/server.js
+++ client/src/server.js
@@ -3,7 +3,9 @@ require('dotenv').config({path:"/Users/jjomyi/Desktop/Cyphers/client/.env"});
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const request = require('request');
 const server = require('http').createServer(app);
+const cron = require('node-cron')
 // CORS 사용
 app.use(cors());
 // mysql 연동
@@ -20,164 +22,128 @@ const con = mysql.createConnection({
   password: DB_PW,
   database: DB_NAME
 })
-//통합랭킹 조회 api
 con.connect((err) => {
-  if(err) {
-    console.log(err)
-  }
-  console.log('DB연결 Connected')
-
-const MAX_LIMIT = 1000; // 최대 limit 값
-const totalData = []; // 데이터를 저장할 배열
-
-// function fetchData(offset) {
-//   let request = require('request')
-//   const apiRequestUserinfo = {
-//     method: 'GET',
-//     url: `https://api.neople.co.kr/cy/ranking/ratingpoint?offset=${offset}&limit=${MAX_LIMIT}&apikey=${APIKEY}`,
-//   };
-
-//   request(apiRequestUserinfo, (err, response) => {
-//     if (err) {
-//       console.log(err);
-//       return;
-//     }
-//     const userinfo = JSON.parse(response.body);
-
-//     // 데이터를 배열에 추가
-//     totalData.push(...userinfo.rows);
-//     // 다음 페이지 데이터 가져오기
-//     if (userinfo.rows.length === MAX_LIMIT) {
-//       fetchData(offset + MAX_LIMIT);
-//     } else {
-
-//       // 모든 데이터를 가져왔을 때 DB에 저장하거나 다른 처리를 수행할 수 있습니다.
-//       // totalData 배열에는 모든 데이터가 저장됩니다.
-//       console.log('모든 데이터를 가져왔습니다.');
-//       // console.log('총 데이터 개수:', totalData.length);
-//       for(let a = 0; a<totalData.length; a++) {
-//         const playerId = totalData[a].playerId
-//         const nickname = totalData[a].nickname
-//         // console.log(playerId)
-        
-//         // app.get('/res', (req, res) => {
-//         //   // totalData 배열의 모든 nickname 값을 클라이언트에게 반환
-//         //   const nicknames = totalData.map((data) => data.nickname);
-//         //   res.send(nicknames);
-//         // });
-        
-//         // const checkDuplicateQuery = `SELECT * FROM Cyphers.testusers WHERE playid = '${playerId}' AND name = '${nickname}`;
-//         const checkDuplicateQuery = `SELECT * FROM Cyphers.testusers2 WHERE playid = '${playerId}' AND name = '${nickname}'`;
-//         // const checkDuplicateQuery = `SELECT * FROM Cyphers.testusers2 WHERE playid = '${playerId}'`;
-
-
-//         con.query(checkDuplicateQuery,(err,rows) => {
-//           if(err) {
-//             console.log(err)
-//           } else {
-//             if(rows.length === 0) {
-//               const insertQuery = `INSERT INTO Cyphers.testusers2 (playid, name, createdDate) VALUES ('${playerId}', '${nickname}', NOW())`;
-
-//               con.query(insertQuery,(err,result) => {
-//                 if(err) {
-//                   console.log(err)
-//                 }
-//                 console.log(`추가된 : playid ${playerId},nickname ${nickname}`,result)
-//               })
-//             } else {
-//               console.log(`playid '${playerId}'는 이미 존재합니다.`);
-//             }
-//           }
-//         })
-
-
-
-
-
-
-
-//         // const sql = `INSERT into Cyphers.testusers(playid,name,createdDate) VALUES('${playerId}','${nickname}',now())`
-//         // con.query(sql,(err,result) => {
-//           // if(err) {
-//           // console.log(err)
-//           // } 
-//           // console.log(result)
-//         // })
-//       }
-//       // 여기에서 DB에 저장하거나 다른 작업을 수행할 수 있습니다.
-//     }
-//   });
-// }
-
-// // fetchData 함수를 호출하여 데이터 가져오기 시작
-// fetchData(0);
-
-
-
-
-
-
-  // const MAX_LIMIT = 1000;
-  // const offset = 0;
-  // const totalData = [];
-
-  // let request = require('request');
-  // const apiRequestUserinfo = {
-  //   'method' : 'GET',
-  //   'url': `https://api.neople.co.kr/cy/ranking/ratingpoint?offset=${offset}&limit=${MAX_LIMIT}&apikey=${APIKEY}`
-  // }
-  // request(apiRequestUserinfo,(err,response) => {
-  //   if(err) {
-  //     console.log(err)
-  //   }
-  //   let userinfo = JSON.parse(response.body)
-    
-    
-  // })
-})
-
-
-
-app.get('/userinfo',(req,res) => {
-  const selectsql = 'select * from Cyphers.testusers2'
-  con.query(selectsql,(err,result) => {
     if(err) {
       console.log(err)
     }
-    res.send(result)
-  })
-})
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// 서버가 잘 동작하고 있는지 확인
-server.listen(8080, ()=>{
-  console.log('server is running on 8080');
+    console.log('DB연결 Connected')
+    processUserData()
 });
 
+ const MAX_LIMIT = 1000;
+ const fetchData = (offset) => {
+    return new Promise((resolve, reject) => {
+        const apiRequestUserinfo = {
+            method: 'GET',
+            url: `https://api.neople.co.kr/cy/ranking/ratingpoint?offset=${offset}&limit=${MAX_LIMIT}&apikey=${APIKEY}`,
+        };
+
+        request(apiRequestUserinfo, (err,response) => {
+            if(err) {
+                reject(err)
+            } else {
+                const userinfo = JSON.parse(response.body)
+                resolve(userinfo)
+            }
+        })
+    })
+ }
+ const processUserData = async () => {
+    try {
+        let offset = 0;
+        const totalData = [];
+
+        while (true) {
+            const userinfo = await fetchData(offset);
+            totalData.push(...userinfo.rows);
+
+            // 다음 페이지 데이터
+            if (userinfo.rows.length !== MAX_LIMIT) {
+                break;
+            }
+            offset += MAX_LIMIT;
+        }
+
+        console.log('모든 데이터를 가져왔습니다. 총 데이터 개수:', totalData.length);
+        for (let a = 0; a < totalData.length; a++) {
+            let playerId = totalData[a].playerId;
+            let nickname = totalData[a].nickname;
+
+            const checkDuplicateQuery = `SELECT * FROM Cyphers.testusers2 WHERE playid = '${playerId}' AND name = '${nickname}'`;
+
+            con.query(checkDuplicateQuery,(err,rows) => {
+                if(err) {
+                    console.log(err)
+                } else {
+                    if(rows.length === 0) {
+                        const insertQuery = `INSERT INTO Cyphers.testusers2 (playid, name, createdDate) VALUES ('${playerId}', '${nickname}', NOW())`;
+                        
+                        con.query(insertQuery,(err,result) => {
+                            if(err) {
+                                console.log(err)
+                            }
+                            console.log(`추가된 : playid : ${playerId},nickname : ${nickname}`)
+                        })
+                    } else {
+                        console.log(`playID는 이미 존재합니다 : ${playerId}`)
+                    }
+                }
+            })
+        }
+    } catch (error) {
+        console.error('데이터를 가져오는 중 에러 발생:', error);
+    }
+};
+
+//1. 개선점 fe에서 넘겨 받은 name을 받아서 쿼리에 변수로 넘겨받아야함 (박현백을 변수로 대체해보셈)
+// app.get('/userinfo',(req, res) => {
+//     const selectQuery =  `
+//         SELECT *
+//         FROM Cyphers.testusers2
+//         WHERE name = ${name}
+//         UNION
+//         SELECT *
+//         FROM Cyphers.testusers2
+//         WHERE playid IN (
+//             SELECT playid
+//             FROM Cyphers.testusers2
+//             WHERE name = "박현백"
+//         );
+//     `;
+//     con.query(selectQuery,(err,result) => {
+//         if(err) {
+//             console.log(err)
+//         }
+//         res.send(result)
+//         //백엔드에서 해야된다
+//     })
+// })
+
+// app.get('/api/username',(req,res) => {
+//     const name = req.query.name
+//         const selectQuery =  `
+//         SELECT *
+//         FROM Cyphers.testusers2
+//         WHERE name = "${name}"
+//         UNION
+//         SELECT *
+//         FROM Cyphers.testusers2
+//         WHERE playid IN (
+//             SELECT playid
+//             FROM Cyphers.testusers2
+//             WHERE name = "${name}"
+//         );
+//     `;
+//     // const selectNameQuery = "SELECT playid , name FROM Cyphers.testusers2 WHERE name = ?"
+//     con.query(selectQuery,[name],(err,result) => {
+//         if(err) {
+//             console.log(err)
+//         }
+//         res.send(result)
+//     })
+// })
+
+server.listen(8080, () => {
+    console.log('server is running on 8080');
+});
 
